89d5f1947fc2251f1760c00d261f4b13fb90644d
Jiachen <xuk4832@gmail.com>
2026-03-21 15:02:11 +0100

    "case"

---
+++ java.js
@@ -0,0 +1,15 @@
+const botonComo = document.querySelector("#como button");
+
+function comojugar(){
+    console.log("jugar");
+}
+
+botonComo.addEventListener("click", comojugar);
+
+const botonJugar = document.querySelector("#jugar button");
+
+function jugar(){
+    console.log("jugarrrrr");
+}
+
+botonJugar.addEventListener("click", jugar);
