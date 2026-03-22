2f446084c451da62981fa442aa6fe8f7e149089a
Jiachen <xuk4832@gmail.com>
2026-03-22 21:38:04 +0100

    java

--- java.js
+++ java.js
@@ -1,10 +1,16 @@
 const botonComo = document.querySelector("#como button");
-
+const buttons = document.querySelector(".buttons");
+const content = document.querySelector("#content");
 function comojugar(){
     console.log("jugar");
+
+    // Mostrar botones
+    buttons.classList.remove("hidden");
+    content.classList.add("hidden");
 }
 
 botonComo.addEventListener("click", comojugar);
+
 
 const botonJugar = document.querySelector("#jugar button");
 
